2d683a5f77c50b6aaf034e710f6287d5ebe3b5e4
AmauryGouverneur <amaury.gouverneur@student.uclouvain.be>
2023-11-28 14:21:42 +0100

    v3.7

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbxHd48CK-bUR75PcZnntHSkAxg3_i07WZWXNqZkyqiVEH1dKhGw2xfj9gZVLh0E0Nq8dg/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzuMIOcNRTlzv4qOv8ikWDCwQfC9S4b_6rdTvB-rV0Jwn0gwRgSPtCXYfl3QtzSYue-RA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
