edc705c4329f0b1b0c754a697a4593877219a955
AmauryGouverneur <amaury.gouverneur@student.uclouvain.be>
2023-12-13 11:28:02 +0100

    new email design

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {*/
-        $.post('https://script.google.com/macros/s/AKfycbwtE3K54ZO5BeE4dSb-rjfJByukTldknb57lbDwY_bJFpF-RkdB13x7x4JaYqHbvezz-g/exec', data)
+        $.post('https://script.google.com/macros/s/AKfycbz2NfmUd0H-tslw1-rMNmcYtYNIzjEfxEFUTUA9CGvbWf_f_V86aUL9GIw8Y_7AdfiMaQ/exec', data)
             .done(function (data) {
                 console.log(data);
                 if (data.result === "error") {
