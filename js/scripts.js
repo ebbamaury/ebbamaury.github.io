6dc0cbf81a237ffbdc78d679e173625d169b8bc4
AmauryGouverneur <amaury.gouverneur@student.uclouvain.be>
2023-12-13 12:44:25 +0100

    font size 500 instead of 600

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {*/
-        $.post('https://script.google.com/macros/s/AKfycbxCirV7_IhjM3X_v1TMK-dQ0LzfRlqNMlok6SGJvpJuvCJXBN_elvBnepX2q19JEeCn1A/exec', data)
+        $.post('https://script.google.com/macros/s/AKfycbwzrO-bTzGERqEFfMviH8eWoI5OajysLVyf5ecQd-X52_Ysw9726wlaqW9sfrrQAfiGaA/exec', data)
             .done(function (data) {
                 console.log(data);
                 if (data.result === "error") {
